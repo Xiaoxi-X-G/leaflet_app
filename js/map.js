5928af065116ac2beaf60386aca38903d5c70853
Xiaoxi Guo <xiaoxi.guo@anz.com>
2017-11-09 13:58:23 +1100

    move leaflet js from webui to leaflet folder

---
+++ js/map.js
@@ -0,0 +1,33 @@
+(function(param){
+    console.log(ausState);
+    geoState = ausState;
+    //console.log(xiaoxi)
+
+      var map = L.map('map').setView([-37.8186804, 144.9479796], 12); 
+      mapLink = 
+                '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+      L.tileLayer(
+                'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: 'Map data &copy; ' + mapLink,
+                maxZoom: 18,
+        }).addTo(map);  
+
+        var marker = L.marker([-37.8186804, 144.9479796],{
+            title: 'Hover text: click',
+            opacity: 0.9, 
+        })
+        .addTo(map)
+        .bindPopup("<b>ANZ</b> <br>My location");
+        //.openPopup();
+
+        L.geoJson(geoState).addTo(map);
+    
+
+    
+})(ausState);
+
+
+//var myFn = function(param){
+//    
+//};
+
